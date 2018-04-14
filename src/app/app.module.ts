462cda534389989bee160c6d86e187dc2d9529fa
santosh <santosh.ece06@gmail.com>
2018-04-14 13:30:53 +0530

    new components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp/comp1/comp1.component';
+import { Comp2Component } from './comp/comp2/comp2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
   ],
   imports: [
     BrowserModule
